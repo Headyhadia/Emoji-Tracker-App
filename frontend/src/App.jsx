f459c6f351f532d0ab3e64cae9c8f8c33bc66d6a
Hadia <hadiaabq@gmail.com>
2025-12-09 16:06:57 +0500

    Import Emojis and EmojiCounts components into homePage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-//import { useState } from "react";
 import "./App.css";
-import Emojis from "./components/Emojis/Emojis";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import EmojiCounts from "./components/EmojiCounts/EmojiCounts.jsx";
+import HomePage from "./pages/HomePage/homePage.jsx";
 function App() {
   return (
     <>
       <Navbar />
-      <Emojis />
-      <EmojiCounts />
+      <HomePage />
     </>
   );
 }
---
+++ frontend/src/pages/HomePage/homePage.jsx
@@ -0,0 +1,13 @@
+import Emojis from "../components/Emojis/Emojis";
+import EmojiCounts from "../components/EmojiCounts/EmojiCounts";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Emojis />
+      <EmojiCounts />
+    </div>
+  );
+};
+
+export default HomePage;
