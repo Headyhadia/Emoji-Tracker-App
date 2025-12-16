3e6344b2f9fd8c1f111844e996e5e7a4e026f052
Hadia <hadiaabq@gmail.com>
2025-12-16 16:22:13 +0500

    feat(frontend): Change emojis

--- frontend/src/components/DefaultEmoji/DefaultEmoji.jsx
+++ frontend/src/components/DefaultEmoji/DefaultEmoji.jsx
@@ -1,6 +1,24 @@
 import styles from "./DefaultEmoji.module.css";
+import { Switch, FormControlLabel } from "@mui/material";
+import { useState } from "react";
 
 const DefaultEmoji = () => {
-  return <div className={styles.defaultEmojiContainer}></div>;
+  const [enabled, setEnabled] = useState(false);
+  return (
+    <div className={styles.defaultEmojiContainer}>
+      <div className={styles.headingBtnContainer}>
+        <h2>DEFAULT EMOJI</h2>
+        <FormControlLabel
+          className={styles.switch}
+          control={
+            <Switch
+              checked={enabled}
+              onChange={(e) => setEnabled(e.target.checked)}
+            />
+          }
+        />
+      </div>
+    </div>
+  );
 };
 export default DefaultEmoji;
